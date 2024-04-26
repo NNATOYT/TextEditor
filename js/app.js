d3b00faeb8f2a5182411febb00ce0fbcee3ca758
NNATOYT <nnatostream@gmail.com>
2024-04-25 20:38:31 -0500

    Notes not collapse

--- js/app.js
+++ js/app.js
@@ -4,5 +4,11 @@ import {Note} from "./models/note.js"
 const btn = document.getElementById("btn-save-editor")
 
 btn.addEventListener("click" , () => {
-    const note = new Note()
+    const name = document.getElementById("note-name-editor")
+    const text = document.getElementById("note-text-editor")
+
+    const note = new Note(name.value,text.value)
+    const ui = new UI()
+
+    ui.cerateNewNote(note)
 })
--- js/models/ui.js
+++ js/models/ui.js
@@ -9,13 +9,20 @@ export class UI {
     @param {Note} note The note to add on the Document
     */
     cerateNewNote(note) {
+        const notesContainer = document.getElementById("notes-container")
         const element = document.createElement("div")
+        const buttonDelete = document.createElement("button")
+
+        buttonDelete.innerText = "Delete"
+        buttonDelete.addEventListener("click",() => element.remove())
+
+        element.classList = "note"
         element.innerHTML = `
-            <b>Name:${this.name}</b>
-            <br>
-            <span>${this.text}</span>
-            <br>
+            <b>Name:${note.name}</b>
+            <span class="text">${note.text}</span>
             <span>Date: 10/10/2024</span>
         `
+        element.append(buttonDelete)
+        notesContainer.append(element)
     }
 }
